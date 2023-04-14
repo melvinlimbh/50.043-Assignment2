378c278039f369befa56c2427252d86d3c394142
Melvin Lim <melvinlimbh@gmail.com>
2023-04-14 23:55:53 +0800

    a

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -18,9 +18,8 @@ csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.c
 
 df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
 
-df2.filter(df2["Rating"] > 1.0)
+df2.filter(df2["Rating"] > 1.0).filter(df2["Reviews"].contains(alphabets))
 df2.write.csv("hdfs:///assignment2/output/question1/output.csv", header=True)
 spark.read.csv("hdfs:///assignment2/output/question1/output.csv",header=True, inferSchema= True).show()
-print("==============print statements==================")
+# print("==============print statements==================")
 # df2.foreach(f)
-print(type(df2["Reviews"]))
