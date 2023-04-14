802e40695bbb195a3d3cd5d80265b8574b5d0d2a
Melvin Lim <melvinlimbh@gmail.com>
2023-04-14 23:05:34 +0800

    a

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -11,4 +11,5 @@ spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
 
 df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
-df2.filter(df2["Rating"] == 1.0).filter(df2["Price Range"] != "null").filter(df2["Reviews"] != []).show()
+df2.filter(df2["Rating"] == 1.0).filter(df2["Price Range"] != "null").show()
+print("ADVASRGFSGASF!!!!!!!!_____++++++++================")
