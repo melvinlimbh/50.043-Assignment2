6d8e2f2badb9390b14a8fe8dc3310abdbfef724e
Melvin Lim <melvinlimbh@gmail.com>
2023-04-14 21:44:55 +0800

    a

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -8,7 +8,7 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
-cv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
-df2 = spark.read.csv(cv_file_path, header=True, inferSchema=True)
+csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
+df2 = spark.read.csv(csv_file_path, header="rating" < 1.0, inferSchema=True)
 df2.printSchema()
 df2.show()
