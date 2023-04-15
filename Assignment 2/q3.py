6a992402f9fb73f1cc4ebb1058803c532712c182
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 15:25:29 +0800

    a

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -9,10 +9,13 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+def f(row):
+    return list[row][-1]
 sc = spark.sparkContext
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
 df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
-date = df2.select(df2["Reviews"]).toDF("Date").show()
+date = df2.select(df2["Reviews"]).toDF("Date")
+date.foreach(f)
 
 
 # moddata = [("review", "Date")]
