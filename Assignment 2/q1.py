50c78a122af92b14a86eefc815422339697edd63
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 16:44:09 +0800

    a

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -9,15 +9,11 @@ hdfs_nn = sys.argv[1]
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# def f(row):
-#     if (row["Rating"] < 1.0) or (alphabets not in row["Reviews"]):
-#         row.show()
-
 
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
 
-df2 = spark.read.csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn), header= True, inferSchema=True)
+df2 = spark.read.csv("hdfs:///assignment2/part1/input/" % (hdfs_nn), header= True, inferSchema=True)
 
 df2.filter(df2["Rating"] >= 1.0).filter(df2["Reviews"].isNotNull())
-df2.write.csv("hdfs://%s:9000/assignment2/output/question1/" % (hdfs_nn), header=True)
+df2.write.csv("hdfs:///assignment2/output/question1/" % (hdfs_nn), header=True)
 #spark.read.csv("hdfs:///assignment2/output/question1/output.csv",header=True, inferSchema= True).show()
