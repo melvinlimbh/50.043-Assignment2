975b53fc3282be33cb07d98b5dce8e02b3a602d9
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 14:17:23 +0800

    a

--- Assignment 2/q2.py
+++ Assignment 2/q2.py
@@ -14,8 +14,11 @@ find distinct cities, filter by lowest and highest price ranges that are not nul
 """
 
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
-df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True).show()
-print("AFTER")
-df3 = df2.filter(df2["Price Range"] != " ").show()
+df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
+print("=================BEFORE=================")
+df2.show()
+print("=================AFTER=================")
+df2.filter(df2["Price Range"] != " ").distinct(df2["Price Range"])
+df2.show()
 # print("MIN")
 # df4 = df2.groupBy(df2["City"]).min().show()
