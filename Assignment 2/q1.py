69bdd3d6744fe255ab07a359df1762e0085b7d81
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 16:52:25 +0800

    a

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -12,7 +12,7 @@ alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
 
-df2 = spark.read.csv("hdfs:///assignment2/part1/input/", header= True, inferSchema=True)
+df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
 
 df2.filter(df2["Rating"] >= 1.0).filter(df2["Reviews"].isNotNull())
 df2.write.csv("hdfs:///assignment2/output/question1/", header=True)
