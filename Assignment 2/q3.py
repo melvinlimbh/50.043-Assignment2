187a42aa33d88c2dd583630305f76fbe16a5571b
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 22:36:33 +0800

    a

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -24,6 +24,21 @@ df2 = df2.withColumn("Review",reviews_and_dates.getItem(0)).withColumn("Date",re
 df2 = df2.withColumn("Review", split(df2["Review"], "\\', \\'")).withColumn(
     "Date", split(df2["Date"], "\\', \\'"))
     
-df2.show()
+# df2.show()
 
 new_df = df2.withColumn("new", arrays_zip("review", "date")).withColumn("new", explode("new")).select("ID_TA", col("new.review").alias("review"), col("new.date").alias("date"))
+
+new_df = new_df.withColumn("review", regexp_replace("review", "'", "")).withColumn(
+    "date", regexp_replace("date", "'", "")
+)
+
+new_df = new_df.withColumn("review", regexp_replace("review", "\\[", "")).withColumn(
+    "date", regexp_replace("date", "\\]", "")
+)
+
+# Remove useless leading and trailing whitespace characters
+new_df = new_df.withColumn("review", trim(new_df.review)).withColumn(
+    "date", trim(new_df.date)
+)
+
+new_df.show()
