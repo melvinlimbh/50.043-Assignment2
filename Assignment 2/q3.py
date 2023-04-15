cd7b829bdac76095bbf3e69e1e80adf474cf6c64
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 15:14:58 +0800

    a

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -9,16 +9,13 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
-
 sc = spark.sparkContext
 csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
-
 df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
-#date = df2.select(df2["Reviews"])
+date = df2.select(df2["Reviews"]).toDF("Date").show()
 
 
-moddata = [("review", "Date")]
-distmodData = sc.parallelize(moddata)
-moddf = distmodData.toDF(["review", "Date"])
-df2.join(moddf, "inner")\
-   .select(df2["ID_TA"]).show()
+# moddata = [("review", "Date")]
+# distmodData = sc.parallelize(moddata)
+# moddf = distmodData.toDF(["review", "Date"])
+# df2.join(moddf, "inner").select(df2["ID_TA"]).show()
