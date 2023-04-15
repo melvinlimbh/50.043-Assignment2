dc3576f77ca05dd480af4355a068c575939d122a
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 13:51:54 +0800

    a

--- Assignment 2/q2.py
+++ Assignment 2/q2.py
@@ -9,3 +9,9 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
+df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
+print("MAX")
+df3 = df2.groupBy(df2["City"]).max().show()
+print("MIN")
+df4 = df2.groupBy(df2["City"]).min().show()
