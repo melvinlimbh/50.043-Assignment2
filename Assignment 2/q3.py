9b984798873f5407c4c3a5842993e6daf046c6a5
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 23:20:48 +0800

    a

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -19,19 +19,18 @@ df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
 
 reviews_and_dates = split(df2["Reviews"],"\\], \\[")
 df2 = df2.withColumn("review",reviews_and_dates.getItem(0)).withColumn("date",reviews_and_dates.getItem(1))
+# add into dataframe
+df2 = df2.withColumn("review", split(df2["review"], "\\', \\'")).withColumn("date", split(df2["date"], "\\', \\'"))
 # now is multiple reviews and dates in 1 line -> split again
 
-df2 = df2.withColumn("review", split(df2["review"], "\\', \\'")).withColumn("date", split(df2["date"], "\\', \\'"))
+#create new dataframe with the 3 columns 
+new_df = df2.withColumn("new", arrays_zip("review", "date")).withColumn("new", explode("new")).select("ID_TA", df2["new.review"].alias("review"), df2["new.date"].alias("date"))
 
-df2.show()
-
-new_df = df2.withColumn("new", arrays_zip("review", "date")).withColumn("new", explode("new")).select("ID_TA", col("new.review").alias("review"), col("new.date").alias("date"))
-
+#remove inverted commas and square brackets
 new_df = new_df.withColumn("review", regexp_replace("review", "'", "")).withColumn("date", regexp_replace("date", "'", ""))
-
 new_df = new_df.withColumn("review", regexp_replace("review", "\\[", "")).withColumn("date", regexp_replace("date", "\\]", ""))
 
-# remove leading/trailing whitespace characters
-new_df = new_df.withColumn("review", trim(new_df.review)).withColumn("date", trim(new_df.date))
 
-# new_df.show()
+# remove leading/trailing whitespace
+new_df = new_df.withColumn("review", trim(new_df.review)).withColumn("date", trim(new_df.date))
+new_df.show()
