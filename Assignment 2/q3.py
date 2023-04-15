fb16e715e6ea0358e53dd545f5e89d9dce5920df
Melvin Lim <melvinlimbh@gmail.com>
2023-04-15 22:54:54 +0800

    a

--- Assignment 2/q3.py
+++ Assignment 2/q3.py
@@ -18,27 +18,20 @@ csv_file_path ="hdfs:///assignment2/part1/input/TA_restaurants_curated_cleaned.c
 df2 = spark.read.csv(csv_file_path, header= True, inferSchema=True)
 
 reviews_and_dates = split(df2["Reviews"],"\\], \\[")
-df2 = df2.withColumn("Review",reviews_and_dates.getItem(0)).withColumn("Date",reviews_and_dates.getItem(1))
+df2 = df2.withColumn("review",reviews_and_dates.getItem(0)).withColumn("date",reviews_and_dates.getItem(1))
 # now is multiple reviews and dates in 1 line -> split again
 
-df2 = df2.withColumn("Review", split(df2["Review"], "\\', \\'")).withColumn(
-    "Date", split(df2["Date"], "\\', \\'"))
-    
-# df2.show()
+df2 = df2.withColumn("review", split(df2["review"], "\\', \\'")).withColumn("date", split(df2["date"], "\\', \\'"))
+
+df2.show()
 
 new_df = df2.withColumn("new", arrays_zip("review", "date")).withColumn("new", explode("new")).select("ID_TA", col("new.review").alias("review"), col("new.date").alias("date"))
 
-new_df = new_df.withColumn("review", regexp_replace("review", "'", "")).withColumn(
-    "date", regexp_replace("date", "'", "")
-)
+new_df = new_df.withColumn("review", regexp_replace("review", "'", "")).withColumn("date", regexp_replace("date", "'", ""))
 
-new_df = new_df.withColumn("review", regexp_replace("review", "\\[", "")).withColumn(
-    "date", regexp_replace("date", "\\]", "")
-)
+new_df = new_df.withColumn("review", regexp_replace("review", "\\[", "")).withColumn("date", regexp_replace("date", "\\]", ""))
 
-# Remove useless leading and trailing whitespace characters
-new_df = new_df.withColumn("review", trim(new_df.review)).withColumn(
-    "date", trim(new_df.date)
-)
+# remove leading/trailing whitespace characters
+new_df = new_df.withColumn("review", trim(new_df.review)).withColumn("date", trim(new_df.date))
 
-new_df.show()
+# new_df.show()
