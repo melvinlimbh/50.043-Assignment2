b332aa0560e26136a9815566586916f4bd58b993
jodi <63439789+jodiwu@users.noreply.github.com>
2023-04-16 22:04:51 +0800

    update

--- q5.py
+++ q5.py
@@ -1,54 +1,50 @@
 import sys 
 from pyspark.sql import SparkSession
 # you may add more import if you need to
-from pyspark.sql.functions import  explode, count, col,  from_json, concat_ws, least, greatest
-
+from pyspark.sql.functions import from_json, explode, col, array, array_sort
+from pyspark.sql.types import ArrayType, StructType, StructField, StringType
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 5").getOrCreate()
 # YOUR CODE GOES BELOW
+schema = ArrayType(StructType([
+    StructField("name", StringType())
+]))
+
 df = (
     spark.read.option("header", True)
-    .option("inferSchema", True)
     .option("delimiter", ",")
+    .option("inferSchema", True)
     .option("quotes", '"')
-    .parquet("hdfs://%s:9000/assignment2/part2/input/" % (hdfs_nn))
+    .parquet("hdfs://%s:9000/assignment2/part2/input/tmdb_5000_credits.parquet" % hdfs_nn)
 )
 
-json_parser = "array<struct<name:string>>"
-
-
-#hint 
 df = df.drop("crew")
-df = df.withColumn(
-    "actor1", explode(from_json(col("cast"), json_parser).getField("name"))
-)
-df = df.withColumn(
-    "actor2", explode(from_json(col("cast"), json_parser).getField("name"))
-)
-df=df.select("movie_id","title","actor1","actor2").filter(col("actor1") != col("actor2"))
 
+df = df.withColumn("actor1", explode(from_json(col("cast"), schema).getField("name"))).withColumn("actor2", explode(from_json(col("cast"), schema).getField("name")))
 
-df = df.withColumn("helper", concat_ws(",", least("actor1", "actor2"), greatest("actor1", "actor2")))
-new_df = df.dropDuplicates(["movie_id", "title", "helper"]).sort(col("helper").asc())
+df = df.select("movie_id", "title", "actor1", "actor2").filter(col("actor1") != col("actor2"))
+
+df = df.withColumn("actors", array(col("actor1"), col("actor2"))).withColumn("actors", array_sort(col("actors")).cast("string"))
+
+df = df.dropDuplicates(["movie_id", "title", "actors"]).sort(col("actors").asc())
 
 df_counter = (
-    new_df.groupBy("helper")
-    .agg(count("*").alias("count"))
+    df.groupBy("actors")
+    .count()
     .filter(col("count") > 1)
 )
 
-final_df = (
-    df_counter.join(new_df, ["helper"], "inner")
-    .sort(col("helper").asc())
-    .drop("helper", "count")
-
+new_df = (
+    df_counter.join(df, ["actors"], "inner")
+    .sort(col("actors").asc())
+    .drop("actors", "count")
 )
-final_df.show()
-rows = final_df.count()
-print("\nrows:", rows)
-final_df.write.option("header", True).mode("overwrite").parquet(
+
+new_df.show()
+
+new_df.write.option("header", True).mode("overwrite").parquet(
     "hdfs://%s:9000/assignment2/output/question5/" % (hdfs_nn)
 )
